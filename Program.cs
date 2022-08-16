541f812db3c38f0b76a62580b6c0d12165f23164
JOSUE RAFAEL GOMEZ DIAZ <SMSS108122@ugb.edu.sv>
2022-08-15 21:31:22 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("introduzca un numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduzca otro numero");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("la suma es " + suma(num1, num2));
+            Console.WriteLine("la resta es " + Resta(num1, num2));
+            Console.WriteLine("la multiplicacion es " + multi(num1, num2));
+            Console.WriteLine("la division es " + div(num1, num2));
+
+        }
+        static int suma(int num1, int num2);
+        
+            
+                var
+                    suma = num1 + num2
+                return suma
+
+            
+            static int Resta(int num1, int num2);
+            
+                var
+                    resta = num1 - num2;
+                return resta;
+
+            }
+            static int multi(int num1, int num2) => num1 * num2;
+            static double Div(int num1, int num2);
+            
+        double numd1 = Convert.ToDouble(num1);
+        double numd2 = Convert.ToDouble(numm2);
+        return numd1 / numd2;
+            
+
+        
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+         }
+    }
+}
